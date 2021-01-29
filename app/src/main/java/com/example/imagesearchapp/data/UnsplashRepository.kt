1dd59784eb6af65fc34c66525a58e487c812edcb
pal <vaibhav171997@gmail.com>
2021-01-30 00:26:51 +0530

    Integrated Dependency injection with dagger-hilt

---
+++ app/src/main/java/com/example/imagesearchapp/data/UnsplashRepository.kt
@@ -0,0 +1,9 @@
+package com.example.imagesearchapp.data
+
+import com.example.imagesearchapp.api.UnsplashApi
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class UnsplashRepository @Inject constructor(private val unsplashApi: UnsplashApi) {
+}
--- app/src/main/java/com/example/imagesearchapp/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/example/imagesearchapp/ui/gallery/GalleryFragment.kt
@@ -2,6 +2,8 @@ package com.example.imagesearchapp.ui.gallery
 
 import androidx.fragment.app.Fragment
 import com.example.imagesearchapp.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class GalleryFragment : Fragment(R.layout.fragment_gallery) {
 }
